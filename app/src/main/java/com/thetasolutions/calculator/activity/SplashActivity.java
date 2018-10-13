85c424cab738e1a46ca0c24223c9685c6cfda348
Waseem Abbas <wajutt8@gmail.com>
2018-10-13 15:42:32 +0500

    splash screen

---
+++ app/src/main/java/com/thetasolutions/calculator/activity/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.thetasolutions.calculator.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.thetasolutions.calculator.R;
+
+public class SplashActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnOpenCal;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        btnOpenCal=findViewById(R.id.btn_OpenCalculator);
+        btnOpenCal.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        // opens next activity
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
